9836b8e2f5b9616b6ed12f6a2d0574548c87faa1
Bjarne L. Gram <bjarne@microgram.no>
2022-10-13 13:19:42 +0200

    added filters

--- Restore-SPOnlineFiles.ps1
+++ Restore-SPOnlineFiles.ps1
@@ -26,7 +26,7 @@ $menuItems = @(
     [InteractiveMultiMenuItem]::new("user", "Deleted by user", $false, 0, $false, "Restore files deleted by specified user.")
     [InteractiveMultiMenuItem]::new("onedate", "Deleted on date", $false, 1, $false, "Restore files deleted on a specific date.")
     [InteractiveMultiMenuItem]::new("datespan", "Deleted date span", $false, 1, $false, "Restore files deleted between two dates.")
-    [InteractiveMultiMenuItem]::new("filetype", "File type", $false, 1, $false, "Restore files of a specified filetype.")
+    [InteractiveMultiMenuItem]::new("filetype", "File type", $false, 1, $false, "Restore files of a specified filetype (one file type only).")
 )
 
 WriteHeader "Script to restore files from SharePoint online recycle bin"
@@ -65,4 +65,31 @@ $menu.SetOptions($options)
 # Trigger the menu and receive the user selections
 $selectedItems = $menu.GetSelections()
 
-Get-PnPRecycleBinItem | ? {($_.DeletedDate -gt $date2 -and $_.DeletedDate -lt $date1) -and ($_.DeletedByEmail -eq 'john@contoso.com')}  | select -last 10 | fl *
+# Set up the filter string
+$pnpFilter = ""
+$userName = ""
+
+foreach ($item in $selectedItems)
+{
+    if($item -eq "user")
+    {
+        $userName = Ask "Input the users email adress (of the user that deleted the files):"
+    }
+
+    if($item -eq "onedate")
+    {
+        $deletedDate = Ask "Input the date files were deleted [[yyyy-mm-dd]:"
+    }
+
+    if($item -eq "datespan")
+    {
+        $fromDate = Ask "Input the date the files were deleted after (from date [yyyy-mm-dd]):"
+        $toDate = Ask "Input the date the files were deleted before (to date [yyyy-mm-dd]):"
+    }
+
+    if($item -eq "filetype")
+    {
+        $fileType = Ask "Which type of files are to be restored (*.ext - one file type only):"
+    }
+}
+# Get-PnPRecycleBinItem | ? {($_.DeletedDate -gt $date2 -and $_.DeletedDate -lt $date1) -and ($_.DeletedByEmail -eq 'john@contoso.com')}  | select -last 10 | fl *
