656fb4d80d73c0a2790c48e858065cc5528fe7ef
Bjarne L. Gram <bjarne@microgram.no>
2022-10-17 13:56:25 +0200

    Rename ClearTeamsCache.ps1 to Clear-TeamsCache.ps1

--- ClearTeamsCache.ps1
+++
@@ -1,1 +0,0 @@
-spps -Name Teams -ErrorAction SilentlyContinue; gci $env:AppData\Microsoft\Teams -Directory | ? { $_ -in ('Cache','databases','blob_storage','IndexedDB','GPUcache', 'Local Storage', 'tmp') } | % { Remove-Item $_.FullName -Recurse -Force -ErrorAction SilentlyContinue}
